72cedf68770f777abfde1765299348f85e56fa76
dhilansepta <dhilan.septa80@gmail.com>
2023-11-27 01:37:33 +0700

    fix logic pada saat menambahkan data penjualan

--- tambah_penjualan.php
+++ tambah_penjualan.php
@@ -14,21 +14,14 @@ while ($row_produk = mysqli_fetch_assoc($query_produk)) {
 if (isset($_POST['submit'])) {
     $np = $_POST['nama_produk'];
     $hrgjual = $_POST['harga_jual'];
-    // $hrgmodal = $_POST['harga_modal'];
     $qty = $_POST['kuantitas'];
     $date = $_POST['tgl'];
-
-    $q = mysqli_query($conn, "INSERT INTO penjualan (nama_produk, harga_jual, kuantitas, tgl) VALUES ('$np', '$hrgjual', '$qty','$date')");
-
-    if ($q) {
-        header('Location: penjualan.php');
-    } else {
-        echo "<script>alert('Gagal menambahkan data'); window.location.href = penjualan.php;</script>";
-    }
     
     $selectedOption = mysqli_fetch_assoc(mysqli_query($conn, "SELECT composition FROM products WHERE product_name = '$np'"));
 
     $komposisi = json_decode($selectedOption['composition'], true);
+    $isValid = true;
+
     foreach ($komposisi as $key => $value) {
         // Memeriksa apakah kunci mengandung substring "bahan"
         if (strpos($key, 'bahan') !== false) {
@@ -42,7 +35,7 @@ if (isset($_POST['submit'])) {
             $result = mysqli_query($conn, $query);
     
             if ($result) {
-                // Ambil hasil kueri
+                // Ambil hasil query
                 $row = mysqli_fetch_assoc($result);
                 $jumlahDiDatabase = $row['jumlah_bahan'];
     
@@ -51,13 +44,33 @@ if (isset($_POST['submit'])) {
                     
                     $updateQuery = "UPDATE bahan SET jumlah_bahan = '$jumlahDiDatabase' WHERE nama_bahan = '$bahan'";
                     $updateResult = mysqli_query($conn, $updateQuery);
+
                     if (!$updateResult) {
                         echo "<script>alert('Gagal mengupdate stok bahan');</script>";
+                        $isValid = false;
                     }
+
+                } else if ($totalBahanTerpakai == $jumlahDiDatabase){
+                    $updateQuery1 = "UPDATE bahan SET jumlah_bahan = '$jumlahDiDatabase' WHERE nama_bahan = '$bahan'";
+                    $updateResult1 = mysqli_query($conn, $updateQuery1);
+                    $updateQuery2 = "DELETE FROM bahan WHERE nama_bahan = '$bahan'";
+                    $updateResult2 = mysqli_query($conn, $updateQuery2);
+
                 } else {
-                    echo "<script>alert('Stok bahan tidak mencukupi'); window.location.href = penjualan.php;</script>";
+                    $isValid = false;
+                    echo "<script>alert('Gagal menambahkan data penjualan'); window.location.href = 'penjualan.php';</script>";
                 }
             }
+        }
+    }
+
+    if ($isValid) {
+        $q = mysqli_query($conn, "INSERT INTO penjualan (nama_produk, harga_jual, kuantitas, tgl) VALUES ('$np', '$hrgjual', '$qty','$date')");
+
+        if ($q) {
+            echo "<script>alert('Berhasil tambah data penjualan!'); window.location.href = 'penjualan.php';</script>";
+        } else {
+            echo "<script>alert('Gagal menambahkan data penjualan'); window.location.href = 'penjualan.php';</script>";
         }
     }
 }
@@ -85,10 +98,6 @@ if (isset($_POST['submit'])) {
                 <label for="harga_jual">Harga Jual:</label>
                 <input type="number" class="form-control" id="harga_jual" name="harga_jual" readonly>
             </div>
-            <!-- <div class="form-group">
-                <label for="harga_modal">Harga Modal:</label>
-                <input type="number" class="form-control" id="harga_modal" name="harga_modal">
-            </div> -->
             <div class="form-group">
                 <label for="kuantitas">Jumlah Terjual:</label>
                 <input type="number" class="form-control" id="kuantitas" name="kuantitas">
@@ -97,11 +106,6 @@ if (isset($_POST['submit'])) {
                 <label for="tgl">Tanggal:</label>
                 <input type="date" class="form-control" id="tgl" name="tgl">
             </div>
-            <!-- <div class="form-group">
-                <label for="komposisi">Komposisi:</label>
-                <textarea class="form-control" id="komposisi" name="komposisi" readonly></textarea>
-            </div> -->
-
             <button type="submit" name="submit" class="btn btn-success">Simpan</button>
             <button type="button" class="btn btn-danger" id="cancelButton">Cancel</button>
         </form>
@@ -113,7 +117,7 @@ if (isset($_POST['submit'])) {
     <script>
         // Menambahkan event listener ke tombol "Cancel"
         document.getElementById("cancelButton").addEventListener("click", function () {
-            history.back(); // Menggunakan fungsi history.back() untuk kembali ke halaman sebelumnya
+            window.location.href = 'penjualan.php';
         });
 
         function updateHargaJual() {
